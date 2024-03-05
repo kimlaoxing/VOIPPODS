dd06b8f2369372c52872026a4210f4d4ce5f32e1
kimlaoxing <82880445+kimlaoxing@users.noreply.github.com>
2024-03-05 17:42:17 +0700

    Add files via upload

---
+++ 1.1.2/Voip.xcframework/ios-arm64/Voip.framework/Headers/Voip.h
@@ -0,0 +1,18 @@
+//
+//  Voip.h
+//  Voip
+//
+//  Created by Esekiel Surbakti on 08/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Voip.
+FOUNDATION_EXPORT double VoipVersionNumber;
+
+//! Project version string for Voip.
+FOUNDATION_EXPORT const unsigned char VoipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Voip/PublicHeader.h>
+
+
